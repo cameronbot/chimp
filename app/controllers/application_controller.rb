da3ad0028093aaa518b0917d29147f0ad0e44287
ch <cameron.hill@gmail.com>
2013-04-16 00:07:15 -0700

    heroku debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,9 +5,9 @@ class ApplicationController < ActionController::Base
     context = params[:context] || "tags"
 
     if params[:query].present?
-      where_clause = "CAST(taggings.context AS varchar) like \"%#{context}%\" and name like \"%#{params[:query]}%\""
+      where_clause = "CAST(taggings.context AS varchar) like \"#{context}\" and name like \"%#{params[:query]}%\""
     else
-      where_clause = "CAST(taggings.context AS varchar) like \"%#{context}%\""
+      where_clause = "CAST(taggings.context AS varchar) like \"#{context}\""
     end
 
     @tags = ActsAsTaggableOn::Tag.includes(:taggings).
